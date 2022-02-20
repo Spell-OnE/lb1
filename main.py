9e54eb4c5f59c71506b988b123aed3693804bf58
SpellOnE <salekssuper@mail.ru>
2022-02-20 16:11:36 +0300

    [feat] Added a solution to the quadratic equation

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-sides = sorted(sides, reverse=True)
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-print("Максимальная площадь треугольника", smax)
+import math
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+discr = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % discr)
+
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x1,2 = %.2f" % x)
+else:
+    print("Корней нет")
+
+# disc > 0: a = 14; b = -5; c = -1; disc = 81; x1 = 0,5; x2 = -0.14;
+
+# disc = 0: a = 9; b = -30; c = 25; disc = 0; x1,2 = 1.67
+
+# disc < 0: a = 2; b = 1; c = 67; disc = -535;
